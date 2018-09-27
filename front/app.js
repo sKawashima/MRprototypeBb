cb51a93edb5d000072630df17a5a1558e4625ccc
sKawashima <bysk.333@gmail.com>
2018-09-27 19:51:31 +0900

    曲をピアノ曲に変更

--- front/app.js
+++ front/app.js
@@ -1,13 +1,13 @@
 import 'reset-css'
 import './style.sass'
 
-import E3 from './sound/E3.wav'
 import G3 from './sound/G3.wav'
 import A3 from './sound/A3.wav'
 import B3 from './sound/B3.wav'
 import C4 from './sound/C4.wav'
 import D4 from './sound/D4.wav'
 import E4 from './sound/E4.wav'
+import F4 from './sound/F4.wav'
 
 import Tone from 'tone'
 import Pressure from 'pressure'
@@ -17,9 +17,9 @@ console.log('hello')
 const app = []
 const tone = []
 
-// Thousand Dreamsの旋律
-const soundList = ['E3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4']
-const songScore = ['D4', 'C4', 'A3', 'G3', 'G3', 'A3', 'C4', 'D4', 'E4', 'D4', 'C4', 'A3', 'G3', 'A3', 'C4', 'B3', 'G3', 'E3', 'D4', 'C4', 'A3', 'G3', 'G3', 'A3', 'C4', 'D4', 'E4', 'D4', 'C4', 'A3', 'G3', 'A3', 'C4', 'B3', 'G3', 'A3']
+// 大きな古時計の旋律
+const soundList = ['G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4']
+const songScore = ['G3', 'C4', 'B3', 'C4', 'D4', 'C4', 'D4', 'E4', 'E4', 'F4', 'E4', 'A3', 'D4', 'C4', 'C4', 'C4', 'B3', 'A3', 'B3', 'C4']
 let scoreN = -1
 
 // init
@@ -33,13 +33,13 @@ window.onload = () => {
   app.ctx = app.c.getContext('2d')
   app.ctx.beginPath()
   // setup tone
-  tone['E3'] = new Tone.Player(E3).toMaster()
   tone['G3'] = new Tone.Player(G3).toMaster()
   tone['A3'] = new Tone.Player(A3).toMaster()
   tone['B3'] = new Tone.Player(B3).toMaster()
   tone['C4'] = new Tone.Player(C4).toMaster()
   tone['D4'] = new Tone.Player(D4).toMaster()
   tone['E4'] = new Tone.Player(E4).toMaster()
+  tone['F4'] = new Tone.Player(F4).toMaster()
   console.log(tone)
   // setup methods
   app.c.addEventListener('mousedown', playSound)
@@ -72,7 +72,7 @@ const playSound = () => {
 }
 
 const changeSoundVolume = () => {
-  tone[songScore[scoreN]].volume.value = -(1 - app.force) * 20
+  tone[songScore[scoreN]].volume.value = -(0.5 - app.force) * 20
 }
 
 const stopSound = () => {
